035334727591e7ac0194d693af4ab1c1f99b3b22
cong <cong@gmail.com>
2021-03-09 23:58:56 +0700

    cong fix

--- src/component/CompContent.js
+++ src/component/CompContent.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import CompNotice from './CompNotice'
 
 class CompContent extends React.Component {
     constructor(props){
@@ -10,30 +10,7 @@ class CompContent extends React.Component {
             <section className="content">
   <h5>content</h5>
   <div className="container">
-    <div className="notice">
-      <h3 className="notice-inside" onclick="dropNotice()">
-        <i className="icon--notice" />
-        <a className="notice-title">[Notice] Kakao Easy Subscription Service Temporary Suspension Notice</a>
-      </h3>
-      <div className="notice-content">
-        <p>
-          Hi. This is Fast Campus.<br /><br />
-          Currently, Kakao's simple subscription function is temporarily suspended due 
-          to internal system improvement work.
-          If you have previously signed up for Kakao, you can log in normally.
-          <br /><br />
-          We apologize for any inconvenience this may cause.
-          We will do our best to get the job done as soon as possible.
-          Thank you.
-          <br /><br />
-          Effective date and time
-          <br />
-          ▷ 2021.01.19 (Tuesday) ~ Announcement posted upon completion of improvement
-          <br /><br />
-          2021. 02. 17
-        </p>
-      </div>
-    </div>
+<CompNotice />
     <span className="best">
       BEST
     </span>
